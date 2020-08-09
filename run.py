29d051c4aaf06f89d5b58df3979f23d825a0c785
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-08-09 12:43:09 +0200

    add GUI, and websites runner

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import os
-list = []
-print('ok')
-with open('programList.txt', 'r', encoding='UTF-8') as file:
-    for line in file:
-        element = line.replace("\n", "")
-        print(element)
-        list.append(element)
-for element in list:
-    os.startfile(element)
+# list = []
+# print('ok')
+# with open('programList.txt', 'r', encoding='UTF-8') as file:
+#     for line in file:
+#         element = line.replace("\n", "")
+#         print(element)
+#         list.append(element)
+# for element in list:
+#     os.startfile(element)
+
+os.startfile('C:/Users/lukas/AppData/Local/Programs/Microsoft VS Code/Code.exe')
+os.startfile('C:/Users/lukas/AppData/Local/Programs/Microsoft VS Code/Code.exe')
