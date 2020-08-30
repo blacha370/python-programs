05f447eff88c8c53a62850c4690e0a0a4d954bff
Łukasz Blacha <lukasz.blacha370@gmail.com>
2020-08-30 17:56:48 +0200

    Rewrite AppGui class

--- RunApp/AppGui.py
+++ RunApp/AppGui.py
@@ -1,70 +1,59 @@
 from tkinter import *
 from tkinter import ttk
-# from tkinter import filedialog
-# import win32gui
+import win32gui
+import pywintypes
 
 
 class Gui:
     def __init__(self):
-        self.elements = dict()
+        self.elements = {'root': None, 'main_frame': None, 'data_frame': None, 'buttons_frame': None}
+        self.gui_structure = {
+            'elements': ({'widget': ttk.Frame, 'parent': self.elements['root'], 'name': 'main_frame', 'position': (1, 1)}, {'widget': ttk.Frame, 'parent': self.elements['main_frame'], 'name': 'data_frame', 'position': (1, 1)}, {'widget': ttk.Frame, 'parent': self.elements['main_frame'], 'name': 'buttons_frame', 'position': (2, 1)}, {'widget': ttk.Entry, 'parent': self.elements['data_frame'], 'name': 'url_entry', 'position': (1, 1)},),
+            'buttons': ({'parent': self.elements['buttons_frame'], 'name': "add_page_button", 'command': self.addPage, "text": 'AddPage', "position": (2, 1)}, {'parent': self.elements['buttons_frame'], 'name': "add_program_button", 'command': self.addProgram, "text": 'AddProgram', "position": (2, 2)}, {'parent': self.elements['buttons_frame'], 'name': "exit_button", 'command': self.exitGui, "text": "Run", "position": (2, 3)})}
         self.root = Tk()
         self.elements['root'] = self.root
         self.root.title("Run")
         self.root.columnconfigure(0, weight=1)
 
-        self.createGuiElement(ttk.Frame, self.elements['root'], 'main_frame')
-        self.createGuiElement(ttk.Frame, self.elements['main_frame'], 'data_frame')
-        self.createGuiElement(ttk.Combobox, self.elements['data_frame'], 'combobox')
-        self.createGuiElement(ttk.Frame, self.elements['main_frame'], 'buttons_frame', position=(1, 0))
-        self.createGuiElement(ttk.Button, self.elements['buttons_frame'], 'run_button')
+        for element in self.gui_structure['elements']:
+            self.createGuiElement(element['widget'], element['parent'], element['name'], position=element['position'])
+        for button in self.gui_structure['buttons']:
+            self.createButton(button['parent'], button['name'], button['command'], text=button['text'], position=button['position'])
+        self.createGui()
 
-    def createGuiElement(self, widget_type, parent, element_name, args=dict(), position=(0, 0)):
+    def createGuiElement(self, widget_type, parent, element_name, position=(0, 0)):
         element = widget_type(parent)
         element.grid(column=position[0], row=position[1])
         self.elements[element_name] = element
 
+    def createButton(self, parent, button_name, command, text=str(), position=(0, 0)):
+        element = ttk.Button(parent, command=command, text=text)
+        element.grid(column=position[0], row=position[1])
+        self.elements[button_name] = element
+
     def createGui(self):
         self.root.mainloop()
 
-# s = ttk.Scrollbar(data_frame, orient=HORIZONTAL, command=data_frame.xview)
-# data_frame.configure(xscrollcommand=s.set)
-# s.grid(column=0, sticky='ns')
-#
-# http = StringVar()
-# http_entry = ttk.Entry(data_frame, width=60,
-#                        textvariable=http)
-#
-# http_entry.grid(column=0, row=1, sticky='we')
-#
-# pages_frame = ttk.Frame(data_frame, padding='3 3 3 3')
-# pages_frame.grid(column=0, row=2, sticky='we')
-# pages_frame.columnconfigure(0, weight=1)
-# programs_frame = ttk.Frame(data_frame, padding='3 3 3 3')
-# programs_frame.grid(column=0, row=3, sticky='e')
-#
-# # Buttons
-# buttons_frame = ttk.Frame(main_frame, padding='3 3 3 3', style='BW.TFrame')
-# buttons_frame.grid(column=2, row=1, sticky='nswe')
-# buttons_frame.rowconfigure(3, weight=1)
-#
-# http_links = StringVar()
-#
-# ttk.Button(buttons_frame, text="Add Page", command=addPage,
-#            padding='3 3 3 3').grid(row=1, sticky='nswe')
-# ttk.Button(buttons_frame, text="Add Program", style='darkBtn.TButton',
-#            command=addProgram, padding='3 3 3 3').grid(row=2, sticky='nswe')
-# ttk.Button(buttons_frame, text="Run", command=run,
-#            padding='3 3 3 3').grid(row=3, sticky='swe')
-#
-# # Program options
-# def setProgramOptions(monitors):
-#     buttons_frame.destroy()
-#     settings_frame = ttk.Frame(main_frame, padding = '3 3 3 3', style='BW.TFrame')
-#     monitors_spinbox = ttk.Combobox(settings_frame, values=monitors, state="readonly")
-#     monitors_spinbox.grid()
-#     settings_frame.grid(column=2, row=1, sticky='nswe')
-#
-# setProgramOptions(app.displays.monitors)
+    def addProgram(self):
+        try:
+            path = win32gui.GetOpenFileNameW()
+        except pywintypes.error:
+            path = ''
+        finally:
+            return path
+
+    def addPage(self):
+        url = self.elements['url_entry'].get()
+        self.elements['url_entry'].delete(0, END)
+        if url:
+            if not url.startswith('http://' or 'https://'):
+                url = 'http://' + url
+            return url
+        return
+
+    def exitGui(self):
+        self.root.destroy()
+
 
 a = Gui()
-a.createGui()
+
